2c1c782e7ee9c544ca28e15663fe96766ede15f5
Aigerim Nuralieva <75246512+aiger555@users.noreply.github.com>
2024-01-10 21:33:51 +0600

    Delete HRS directory

--- HRS/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.hrs {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.hrs to javafx.fxml;
-    exports com.example.hrs;
-}
